5bf9c7a55718716986f13539049065be6fcea0d0
patriciabrsk <patriciabresku@gmail.com>
2022-03-03 12:51:42 +0100

    :)

--- js-snack-2/js/script.js
+++ js-snack-2/js/script.js
@@ -29,4 +29,16 @@ function isEvenOrOdd(number) {
     return false;
 }
 
+/* creo un'altra funzione che controlla che la somma di due numeri x e y sia pari
+*
+* sommaPari(24214, 12451241) ==> true se la somma mi dà un numero pari
+*                            ==> false se la somma mi dà un numero dispari
+*/
 
+function isSumEven(x, y) {
+    let sum = x + y;
+    if (sum % 2 == 0) {
+        return true;
+    }
+    return false;
+}
