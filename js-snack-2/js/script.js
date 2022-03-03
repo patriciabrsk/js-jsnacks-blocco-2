c01b17eb53b1cf505aba86488580a899f0a74678
patriciabrsk <patriciabresku@gmail.com>
2022-03-03 12:22:19 +0100

    almost done

---
+++ js-snack-2/js/script.js
@@ -0,0 +1,24 @@
+/**
+ *
+ *
+    Crea due tag div con due id diversi:
+    un div avrà il testo colorato di rosso mentre l'altro di verde.
+    Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
+    Se sono pari li stampo nell'div "rosso",
+    se sono dispari li stampo nell'div "verde".
+ *
+ */
+
+function isEvenOrOdd(arrayNumbers) {
+    const green = document.getElementById('green-text');
+    const red = document.getElementById('red-text');
+
+    // const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = 0; i < arrayNumbers.length; i++) {
+        if (arrayNumbers[i] % 2 === 0) {
+            red.innerHTML += `<p class="text-danger">${arrayNumbers[i]}</p>`;
+        } else {
+            green.innerHTML += `<p class="text-success">${arrayNumbers[i]}</p>`;
+        }
+    }
+}
